0c95b7d09a72fe6d70cf0fe702e79498220bb9f3
libuter <lizaveta.levkovich@gmail.com>
2026-03-10 20:10:24 +0200

    Add files via upload

---
+++ caesar_breaker.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<ctype.h>
+#include<stdlib.h>
+#include<string.h>
+#define LETTERS 26
+
+char decrypt_char(char c, int key){
+    if (isupper(c))
+        return ((c - 'A' - key + LETTERS) % LETTERS) + 'A';
+    return c; // пробелы и другие символы не меняем
+}
+
+void decrypt_with_key(char *cipher, int key){
+    int len = strlen(cipher);
+
+    for (int i = 0; i < len; i++){
+        printf("%c", decrypt_char(cipher[i], key));
+    }
+}
+
+int main(){
+    char cipher[] = "WKLV LV D VHFUHW PHVVDJH";
+
+    printf("Cipher text: %s\n\n", cipher);
+    printf("---- Brute Force Results ----\n");
+
+    for (int key = 1; key < LETTERS; key++){
+        printf("Key %d: ", key);
+        decrypt_with_key(cipher, key);
+        printf("\n");
+    }
+    return EXIT_SUCCESS;
+}
