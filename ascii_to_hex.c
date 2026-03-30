4aec0d90fd3f1f1a5f539def4c8ffe2e46a6fa98
libuter <lizaveta.levkovich@gmail.com>
2026-03-30 21:43:32 +0300

    ascii_to_hex.c

--- ascii_to_hex.c
+++ ascii_to_hex.c
@@ -1,13 +1,14 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 void print_hex(const char *text)
 {
     printf("Symbol | Decimal | Hex\n");
     printf("-------|---------|-----\n");
 
-    for (int i = 0; i < strlen(text); i++){
+    for (int i = 0; i < strlen(text); i++)
+    {
         printf("  %c    |   %3d   | %02X\n", text[i], text[i], text[i]);
     }
 }
