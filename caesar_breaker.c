b4a58f2e9f19e524dcce5b0a7f8f5ba31e9f48dc
libuter <lizaveta.levkovich@gmail.com>
2026-03-30 21:46:31 +0300

    caesar_breaker.c

--- caesar_breaker.c
+++ caesar_breaker.c
@@ -1,20 +1,22 @@
-#include<stdio.h>
-#include<ctype.h>
-#include<stdlib.h>
-#include<string.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
 #define LETTERS 26
 
 char decrypt_char(char c, int key)
 {
     if (isupper(c))
         return ((c - 'A' - key + LETTERS) % LETTERS) + 'A';
-    return c; 
+    return c; // пробелы и другие символы не меняем
 }
 
 void decrypt_with_key(char *cipher, int key)
 {
     int len = strlen(cipher);
-    for (int i = 0; i < len; i++){
+
+    for (int i = 0; i < len; i++)
+    {
         printf("%c", decrypt_char(cipher[i], key));
     }
 }
@@ -26,7 +28,8 @@ int main()
     printf("Cipher text: %s\n\n", cipher);
     printf("---- Brute Force Results ----\n");
 
-    for (int key = 1; key < LETTERS; key++){
+    for (int key = 1; key < LETTERS; key++)
+    {
         printf("Key %d: ", key);
         decrypt_with_key(cipher, key);
         printf("\n");
