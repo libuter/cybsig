cce11eed5dacda732246e0148be459108d171648
libuter <lizaveta.levkovich@gmail.com>
2026-03-30 21:23:46 +0300

    xor_cipher.c

--- xor_cipher.c
+++ xor_cipher.c
@@ -2,20 +2,24 @@
 #include<stdlib.h>
 #include<string.h>
 
-void xor_cipher(char *text, char key){
+void xor_cipher(char *text, char key)
+{
     int len = strlen(text);
-    for (int i = 0; i < len; i++){
+    for (int i = 0; i < len; i++)
+    {
         text[i] ^= key;
     }
 }
 
-int main(){
+int main()
+{
     char data[] = "Secret";
     char key = 'K';
 
     xor_cipher(data, key);
     printf("Encrypted: ");
-    for (int i = 0; i < strlen(data); i++){
+    for (int i = 0; i < strlen(data); i++)
+    {
         printf("%02X ", (unsigned char)data[i]);
     }
     printf("\n");
