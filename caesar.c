9dea139ef4976669a265fda9519a8ac7fefbe2b7
libuter <lizaveta.levkovich@gmail.com>
2026-03-30 21:44:56 +0300

    caesar.c

--- caesar.c
+++ caesar.c
@@ -1,23 +1,29 @@
 #include <stdio.h>
-#include<stdlib.h>
+#include <stdlib.h>
 #include <ctype.h>
 
 char caesar_encrypt_char(char p, int k)
 {
-    if (isupper(p)){
-        return((p - 'A' + k) % 26) + 'A';
-    } else if (islower(p)){
-        return((p - 'a' + k) % 26) + 'a';
+    if (isupper(p))
+    {
+        return ((p - 'A' + k) % 26) + 'A';
+    }
+    else if (islower(p))
+    {
+        return ((p - 'a' + k) % 26) + 'a';
     }
     return p;
 }
 
 char caesar_decrypt_char(char c, int k)
 {
-    if (isupper(c)){
-        return((c - 'A' - k + 26) % 26) + 'A';
-    } else if (islower(c)) {
-        return((c - 'a' - k + 26) % 26) + 'a';
+    if (isupper(c))
+    {
+        return ((c - 'A' - k + 26) % 26) + 'A';
+    }
+    else if (islower(c))
+    {
+        return ((c - 'a' - k + 26) % 26) + 'a';
     }
     return c;
 }
@@ -44,7 +50,7 @@ int main()
     int key = 3;
 
     caesar_encrypt_string(text, key);
-    printf("Encrypted: %s\n", text); 
+    printf("Encrypted: %s\n", text);
 
     caesar_decrypt_string(text, key);
     printf("Decrypted: %s\n", text);
